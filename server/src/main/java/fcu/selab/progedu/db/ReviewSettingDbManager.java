5e596ac2aa44d23627140b1c3128b29ccd69b4fa
chenchunwei24k <chenchunwei24k@gmail.com>
2020-02-06 12:40:25 +0800

    definition method in ReviewSetting

--- server/src/main/java/fcu/selab/progedu/db/ReviewSettingDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewSettingDbManager.java
@@ -6,10 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.sql.Timestamp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fcu.selab.progedu.data.Assignment;
 import fcu.selab.progedu.utils.ExceptionUtil;
 
 public class ReviewSettingDbManager {
@@ -26,4 +29,54 @@ public class ReviewSettingDbManager {
   private ReviewSettingDbManager() {
 
   }
+
+  /**
+   * Add Review_Setting to database
+   * 
+   * @param assignment Project
+   */
+   public void addRevSetting(Assignment assignment) {
+
+   }
+
+  /**
+   * Get Review_Setting Id by Assignment Id
+   *
+   * @param aId Assignment Id
+   * @return Id Review_Setting Id
+   */
+   public int getRevSettingId(int aId) {
+     return 0;
+   }
+
+  /**
+   * Get Review_Setting amount by Assignment Id
+   *
+   * @param aId Assignment Id
+   * @return amount Review_Setting amount
+   */
+   public int getRevSettingAmount(int aId) {
+     return 0;
+   }
+
+  /**
+   * Delete Review_Setting from database
+   * 
+   * @param aId assignment id
+   */
+   public void deleteRevSetting(int aId) {
+
+   }
+
+  /**
+   * Edit Review_Setting
+   * 
+   * @param amount      amount
+   * @param startTime   startTime
+   * @param endTime     endTime
+   * @param id          id
+   */
+   public void editRevSetting(int amount, Date startTime, Date endTime, int id) {
+
+   }
 }
