116015bff9dd22e89fb21eb92f9186b8216144d9
chenchunwei24k <chenchunwei24k@gmail.com>
2020-02-06 16:17:38 +0800

    definition method in ReviewRecord (non-finish)

--- server/src/main/java/fcu/selab/progedu/db/ReviewRecordDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewRecordDbManager.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -26,4 +28,40 @@ public class ReviewRecordDbManager {
   private ReviewRecordDbManager() {
 
   }
+  
+  /**
+   * insert Review_Record into database
+   * 
+   * @param pmId          pmId
+   * @param rId           rId
+   * @param status        status
+   * @param score         score  
+   * @param time          record time
+   * @param feedback      feedback
+   */
+  public void addRevRecord(int pmId, int rId, int status, boolean score, Date time, String feedback) {
+    
+  }
+
+  /**
+   * get Review's Record by pmId and rId
+   *
+   * @param pmId Pair_Matching Id
+   * @param rId Review_Rule Id
+   * @return score which score by designated 
+   */
+   public boolean getRevRecord(int pmId, int rId) {
+     return false;
+   }
+
+  /**
+   * get all Review's Record Lsit by pmId
+   *
+   * @param pmId Pair_Matching pmId
+   * @return lsRecord list of score which score by designated student in designated assignment
+   */
+   public List<boolean> getRevRecordList(int pmId) {
+     List<boolean> lsRecord = new ArrayList<>();
+     return lsRecord;
+   }  
 }
