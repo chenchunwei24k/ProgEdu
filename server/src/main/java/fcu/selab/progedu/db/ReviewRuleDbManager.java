5ef57995931145e218e36fc93891edbd00f7c764
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2020-02-07 17:36:14 +0800

    modified method in ReviewRule

--- server/src/main/java/fcu/selab/progedu/db/ReviewRuleDbManager.java
+++ server/src/main/java/fcu/selab/progedu/db/ReviewRuleDbManager.java
@@ -34,7 +34,23 @@ public class ReviewRuleDbManager {
    * @return Id Review_Rule Id
    */
   public int getRevRuleId(String description) {
-    return 0;
+    String sql = "SELECT id FROM Review_Rule WHERE description = ?";
+    int id = 0;
+
+    try (Connection conn = database.getConnection();
+         PreparedStatement preStmt = conn.prepareStatement(sql)) {
+      preStmt.setString(1, description);
+
+      try (ResultSet rs = preStmt.executeQuery();) {
+        while (rs.next()) {
+          id = rs.getInt("id");
+        }
+      }
+    } catch (SQLException e) {
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
+    }
+    return id;
   }
 
   /**
@@ -44,7 +60,23 @@ public class ReviewRuleDbManager {
    * @return description
    */
   public String getRevRuleDesc(int id) {
-    return "";
+    String sql = "SELECT description FROM Review_Rule WHERE id = ?";
+    String description = "";
+
+    try (Connection conn = database.getConnection();
+         PreparedStatement preStmt = conn.prepareStatement(sql)) {
+      preStmt.setInt(1, id);
+
+      try (ResultSet rs = preStmt.executeQuery();) {
+        while (rs.next()) {
+          description = rs.getString("description");
+        }
+      }
+    } catch (SQLException e) {
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
+    }
+    return description;
   }
 
   /**
@@ -52,7 +84,23 @@ public class ReviewRuleDbManager {
    *
    */
   public List<String> getRevRuleAllDesc() {
+    String sql = "SELECT description FROM Review_Rule";
     List<String> lsDesc = new ArrayList<>();
+
+    try (Connection conn = database.getConnection();
+         PreparedStatement preStmt = conn.prepareStatement(sql)) {
+      preStmt.executeUpdate();
+
+      try (ResultSet rs = preStmt.executeQuery();) {
+        while (rs.next()) {
+          String description = rs.getString("description");
+          lsDesc.add(description);
+        }
+      }
+    } catch (SQLException e) {
+      LOGGER.debug(ExceptionUtil.getErrorInfoFromException(e));
+      LOGGER.error(e.getMessage());
+    }
     return lsDesc;
   }
 
