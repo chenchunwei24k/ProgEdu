69a900dc891f5c1b48b22c2e0788cc5215add6f6
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2020-02-10 14:02:25 +0800

    create ReviewService

---
+++ server/src/main/java/fcu/selab/progedu/service/ReviewService.java
@@ -0,0 +1,36 @@
+package fcu.selab.progedu.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.gitlab.api.models.GitlabProject;
+import org.gitlab.api.models.GitlabUser;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReviewService {
+  private static ReviewService instance = new ReviewService();
+
+  public static ReviewService getInstance() {
+    return instance;
+  }
+
+
+}
